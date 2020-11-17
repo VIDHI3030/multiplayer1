fcc072e544ef406b46c9e6017b2314aad492dd5e
VIDHI3030 <72209528+VIDHI3030@users.noreply.github.com>
2020-11-17 20:04:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,49 +1,19 @@
-var ball;
-var position,database;
+
+var database;
+var gameState=0;
+var form,game,player,playerCount;
 function setup(){
     database=firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-    var dbref=database.ref("ball/position");
-    dbref.on("value",readposition,showerror)
+    game=new Game();
+    game.getstate();
+    game.start();
 
 }
 
 function draw(){
 
-    background("white");
-     if(position !=undefined){
-        if(keyDown(LEFT_ARROW)){
-            changePosition(-1,0);
-        }
-        else if(keyDown(RIGHT_ARROW)){
-            changePosition(1,0);
-        }
-        else if(keyDown(UP_ARROW)){
-            changePosition(0,-1);
-        }
-        else if(keyDown(DOWN_ARROW)){
-            changePosition(0,+1);
-        }
-        
-     }
-  
-    drawSprites();
+    //background("white");
+     
 }
 
-function changePosition(x,y){
-    database.ref('ball/position').set({
-        x: position.x+x,
-         y:position.y+y
-    })
-}
-function readposition(data){
-    position=data.val();
-    ball.x=position.x;
-    ball.y=position.y;
-}
-function showerror(){
-    console.log("could not read database");
-
-}
